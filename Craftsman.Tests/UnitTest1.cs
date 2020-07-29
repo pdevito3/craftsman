ba72de1b429680be1156a1136cff0b83137a8cb9
Paul DeVito <pdevito3@gmail.com>
2020-07-29 00:25:56 -0400

    added test project

---
+++ Craftsman.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace Craftsman.Tests
+{
+    using System;
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
