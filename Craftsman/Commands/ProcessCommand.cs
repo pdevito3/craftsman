ce1391f666405b0739a6fbf3c72ddf78f7887c03
Paul DeVito <pdevito3@gmail.com>
2020-09-06 19:49:00 -0400

    Added Autofac and POC IO Abstraction

---
+++ Craftsman/Commands/ProcessCommand.cs
@@ -0,0 +1,82 @@
+﻿namespace Craftsman.Commands
+{
+    using CommandLine;
+    using Craftsman.CraftsmanOptions;
+    using Craftsman.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.IO.Abstractions;
+
+    public class ProcessCommand
+    {
+        private readonly IFileSystem fileSystem = new FileSystem();
+
+        public void Run(string[] args)
+        {
+            var myEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (args.Length == 0)
+            {
+                ListCommand.Run();
+                return;
+            }
+
+            if (args[0] == "list" || args[0] == "-h" || args[0] == "--help")
+            {
+                ListCommand.Run();
+                return;
+            }
+
+            if (args.Length == 2 && (args[0] == "new:api"))
+            {
+                var filePath = args[1];
+                if (filePath == "-h" || filePath == "--help")
+                    NewApiCommand.Help();
+                else
+                {
+                    var solutionDir = myEnv == "Dev" ? fileSystem.Path.Combine(@"C:","Users","Paul","Documents","testoutput") : fileSystem.Directory.GetCurrentDirectory();
+                    NewApiCommand.Run(filePath, solutionDir);
+                }
+            }
+
+            if (args.Length == 2 && (args[0] == "add:entity"))
+            {
+                var filePath = args[1];
+                if (filePath == "-h" || filePath == "--help")
+                    AddEntityCommand.Help();
+                else
+                {
+                    var solutionDir = myEnv == "Dev" ? fileSystem.Path.Combine(@"C:","Users","Paul","Documents","testoutput") : fileSystem.Directory.GetCurrentDirectory();
+                    AddEntityCommand.Run(filePath, solutionDir);
+                }
+            }
+
+            if (args.Length > 1 && (args[0] == "add:property"))
+            {
+                if (args[1] == "-h" || args[1] == "--help")
+                    AddEntityCommand.Help();
+                else
+                {
+                    var entityName = "";
+                    var newProperty = new EntityProperty();
+                    Parser.Default.ParseArguments<AddPropertyOptions>(args)
+                        .WithParsed(options =>
+                        {
+                            entityName = options.Entity.UppercaseFirstLetter();
+                            newProperty = new EntityProperty()
+                            {
+                                Name = options.Name,
+                                Type = options.Type,
+                                CanFilter = options.CanFilter,
+                                CanSort = options.CanSort,
+                                ForeignKeyPropName = options.ForeignKeyPropName
+                            };
+                        });
+
+                    var solutionDir = myEnv == "Dev" ? fileSystem.Path.Combine(@"C:","Users","Paul","Documents","testoutput") : fileSystem.Directory.GetCurrentDirectory();
+                    AddEntityPropertyCommand.Run(solutionDir, entityName, newProperty);
+                }
+            }
+        }
+    }
+}
