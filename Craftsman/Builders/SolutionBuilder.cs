d698f9fcd0693533b8b7d3285c70b448cf73c857
Paul DeVito <pdevito3@gmail.com>
2020-12-19 11:28:39 -0500

    started domain build

--- Craftsman/Builders/SolutionBuilder.cs
+++ Craftsman/Builders/SolutionBuilder.cs
@@ -7,19 +7,48 @@
     using System.IO;
     using System.Text;
 
-    public static class SolutionBuilder
+    public class SolutionBuilder
     {
         public static void BuildSolution(string solutionDirectory, ApiTemplate template)
         {
             var separator = Path.DirectorySeparatorChar;
+            var domainDirectory = $"{solutionDirectory}{separator}Domain";
+
             Directory.CreateDirectory(solutionDirectory);
             Directory.CreateDirectory($"{solutionDirectory}{separator}Application");
-            Directory.CreateDirectory($"{solutionDirectory}{separator}Domain");
             Directory.CreateDirectory($"{solutionDirectory}{separator}Infrastructure.Persistence");
             Directory.CreateDirectory($"{solutionDirectory}{separator}Infrastructure.Shared");
             Directory.CreateDirectory($"{solutionDirectory}{separator}WebApi");
 
-            Utilities.ExecuteProcess("dotnet", @$"new sln -n {template.SolutionName}", solutionDirectory);
+            try
+            {
+                Utilities.ExecuteProcess("dotnet", @$"new sln -n {template.SolutionName}", solutionDirectory);
+            }
+            catch(Exception e)
+            {
+                // must be using the .net 5 sdk
+            }
+
+            BuildDomainProject(solutionDirectory, domainDirectory);
+            var stopper = true;
+        }
+
+        private static void BuildDomainProject(string solutionDirectory, string domainDirectory)
+        {
+            var separator = Path.DirectorySeparatorChar;
+
+            // domain and app need to be added to a virtual `Common` folder, something like this: dotnet add path/to/project.csproj --solution-folder VirtualFolder
+            Utilities.ExecuteProcess("dotnet", $@"new classlib -n Domain -f netstandard2.1", solutionDirectory);
+
+            // this dir is not getting added to the projec though like you would if you created a new folder in vs...
+            Directory.CreateDirectory($"{domainDirectory}{separator}Entities");
+
+            // remove default class
+            var class1FilePath = Path.Combine(domainDirectory, "Class1.cs");
+            if (File.Exists(class1FilePath))
+                File.Delete(class1FilePath);
+
+            Utilities.ExecuteProcess("dotnet", $@"add Domain package Sieve", solutionDirectory);
         }
     }
 }
