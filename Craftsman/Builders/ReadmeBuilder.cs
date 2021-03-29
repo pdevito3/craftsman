7b37d1cc3cec7d6addb24f1691857203b71fcaef
Paul DeVito <pdevito3@gmail.com>
2021-03-28 21:45:07 -0400

    docsL fixed readme docs for generated project

--- Craftsman/Builders/ReadmeBuilder.cs
+++ Craftsman/Builders/ReadmeBuilder.cs
@@ -108,11 +108,12 @@ To run integration tests:
 
 1. Ensure that you have docker installed.
 2. Go to your src directory: `cd {srcDir}`
-3. Set an environment. It doesn't matter what that environment name is for these purposes.
-    - Powershell: `$Env:ASPNETCORE_ENVIRONMENT = ""IntegrationTesting""`
-    - Bash: export `ASPNETCORE_ENVIRONMENT = IntegrationTesting`
-4. Run a Migration (necessary to set up the database) `dotnet ef migrations add ""InitialMigration"" --project {solutionName}.Infrastructure --startup-project {solutionName}.WebApi --output-dir Migrations`
-5. Run the tests. They will take some time on the first run in the last 24 hours in order to set up the docker configuration.
+3. Confirm that you have migrations in your infrastructure project. If not you can add them by doing the following:
+    1. Set an environment variable. It doesn't matter what that environment name is for these purposes.
+        - Powershell: `$Env:ASPNETCORE_ENVIRONMENT = ""IntegrationTesting""`
+        - Bash: export `ASPNETCORE_ENVIRONMENT = IntegrationTesting`
+    2. Run a Migration (necessary to set up the database) `dotnet ef migrations add ""InitialMigration"" --project {solutionName}.Infrastructure --startup-project {solutionName}.WebApi --output-dir Migrations`
+4. Run the tests. They will take some time on the first run in the last 24 hours in order to set up the docker configuration.
 ";
         }
     }
