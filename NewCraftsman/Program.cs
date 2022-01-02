cde0cc551592db160cc580ff0079d28cfc9c9335
Paul DeVito <pdevito3@gmail.com>
2022-01-01 20:22:30 -0500

    new project base

---
+++ NewCraftsman/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using SpectreFail;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddSingleton<IFileSystem, FileSystem>();
+
+var registrar = new TypeRegistrar(serviceCollection);
+var app = new CommandApp(registrar); // works without registrar
+
+app.Configure(config =>
+{
+    config.AddCommand<HelloCommand>("hello")
+        .WithAlias("hola")
+        .WithDescription("Say hello")
+        .WithExample(new []{"hello", "Phil"})
+        .WithExample(new []{"hello", "Phil", "--count", "4"});
+});
+
+return app.Run(args);
+
+public class HelloCommand : Command<HelloCommand.Settings>
+{
+    private IAnsiConsole _console;
+
+    public HelloCommand(IAnsiConsole console)
+    {
+        _console = console;
+    }
+
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "[Name]")]
+        public string Name { get; set; }
+    }
+
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        // AnsiConsole.MarkupLine($"Hello, [blue]{settings.Name}[/]");
+        _console.MarkupLine($"Hello, [blue]{settings.Name}[/]");
+        return 0;
+    }
+}
---
+++ NewCraftsman/TypeRegistrar.cs
@@ -0,0 +1,59 @@
+namespace SpectreFail;
+
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+public class TypeRegistrar : ITypeRegistrar
+{
+    private readonly IServiceCollection _builder;
+
+    public TypeRegistrar(IServiceCollection builder)
+    {
+        _builder = builder;
+    }
+
+    public ITypeResolver Build()
+    {
+        return new TypeResolver(_builder.BuildServiceProvider());
+    }
+
+    public void Register(Type service, Type implementation)
+    {
+        _builder.AddSingleton(service, implementation);
+    }
+
+    public void RegisterInstance(Type service, object implementation)
+    {
+        _builder.AddSingleton(service, implementation);
+    }
+
+    public void RegisterLazy(Type service, Func<object> func)
+    {
+        if (func is null)
+        {
+            throw new ArgumentNullException(nameof(func));
+        }
+
+        _builder.AddSingleton(service, _ => func());
+    }
+}
+
+public class TypeResolver : ITypeResolver
+{
+    private readonly IServiceProvider _provider;
+
+    public TypeResolver(IServiceProvider provider)
+    {
+        _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+    }
+
+    public object Resolve(Type? type)
+    {
+        if (type == null)
+        {
+            return null;
+        }
+        
+        return _provider.GetService(type);
+    }
+}
