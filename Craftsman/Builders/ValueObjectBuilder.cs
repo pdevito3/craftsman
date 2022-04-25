c0159ef08a8e62732065239b5dda4190885b7d87
Paul DeVito <pdevito3@gmail.com>
2022-04-24 22:12:03 -0400

    fix: value object namespacing

--- Craftsman/Builders/ValueObjectBuilder.cs
+++ Craftsman/Builders/ValueObjectBuilder.cs
@@ -33,100 +33,101 @@ public static class ValueObjectBuilder
         private static string GetFileText(string classNamespace)
         {
             return @$"namespace {classNamespace}
-
-using System.Reflection;
-
-// source: https://github.com/jhewlett/ValueObject
-public abstract class ValueObject : IEquatable<ValueObject>
 {{
-	private List<PropertyInfo>? properties;
-	private List<FieldInfo>? fields;
-	public static bool operator ==(ValueObject? obj1, ValueObject? obj2)
-	{{
-		if (object.Equals(obj1, null))
-		{{
-			if (object.Equals(obj2, null))
-				return true;
+    using System.Reflection;
 
-			return false;
-		}}
+    // source: https://github.com/jhewlett/ValueObject
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {{
+	    private List<PropertyInfo>? properties;
+	    private List<FieldInfo>? fields;
+	    public static bool operator ==(ValueObject? obj1, ValueObject? obj2)
+	    {{
+		    if (object.Equals(obj1, null))
+		    {{
+			    if (object.Equals(obj2, null))
+				    return true;
 
-		return obj1.Equals(obj2);
-	}}
+			    return false;
+		    }}
 
-	public static bool operator !=(ValueObject? obj1, ValueObject? obj2)
-	{{
-		return !(obj1 == obj2);
-	}}
+		    return obj1.Equals(obj2);
+	    }}
 
-	public bool Equals(ValueObject? obj)
-	{{
-		return Equals(obj as object);
-	}}
+	    public static bool operator !=(ValueObject? obj1, ValueObject? obj2)
+	    {{
+		    return !(obj1 == obj2);
+	    }}
 
-	public override bool Equals(object? obj)
-	{{
-		if (obj == null || GetType() != obj.GetType())
-			return false;
-		return GetProperties().All(p => PropertiesAreEqual(obj, p)) && GetFields().All(f => FieldsAreEqual(obj, f));
-	}}
+	    public bool Equals(ValueObject? obj)
+	    {{
+		    return Equals(obj as object);
+	    }}
 
-	private bool PropertiesAreEqual(object obj, PropertyInfo p)
-	{{
-		return object.Equals(p.GetValue(this, null), p.GetValue(obj, null));
-	}}
+	    public override bool Equals(object? obj)
+	    {{
+		    if (obj == null || GetType() != obj.GetType())
+			    return false;
+		    return GetProperties().All(p => PropertiesAreEqual(obj, p)) && GetFields().All(f => FieldsAreEqual(obj, f));
+	    }}
 
-	private bool FieldsAreEqual(object obj, FieldInfo f)
-	{{
-		return object.Equals(f.GetValue(this), f.GetValue(obj));
-	}}
+	    private bool PropertiesAreEqual(object obj, PropertyInfo p)
+	    {{
+		    return object.Equals(p.GetValue(this, null), p.GetValue(obj, null));
+	    }}
 
-	private IEnumerable<PropertyInfo> GetProperties()
-	{{		
-        return this.properties ?? (this.properties = GetType()
-            .GetProperties(BindingFlags.Instance | BindingFlags.Public)
-            .Where(p => p.GetCustomAttribute(typeof(IgnoreMemberAttribute)) == null)
-            .ToList());
-	}}
+	    private bool FieldsAreEqual(object obj, FieldInfo f)
+	    {{
+		    return object.Equals(f.GetValue(this), f.GetValue(obj));
+	    }}
 
-	private IEnumerable<FieldInfo> GetFields()
-	{{
-        return this.fields ?? (this.fields = GetType().GetFields(BindingFlags.Instance | BindingFlags.Public)
-            .Where(p => p.GetCustomAttribute(typeof(IgnoreMemberAttribute)) == null)
-            .ToList());
-	}}
+	    private IEnumerable<PropertyInfo> GetProperties()
+	    {{		
+            return this.properties ?? (this.properties = GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(p => p.GetCustomAttribute(typeof(IgnoreMemberAttribute)) == null)
+                .ToList());
+	    }}
 
-	public override int GetHashCode()
-	{{
-		unchecked //allow overflow
-		{{
-			int hash = 17;
-			foreach (var prop in GetProperties())
-			{{
-				var value = prop.GetValue(this, null);
-				hash = HashValue(hash, value);
-			}}
+	    private IEnumerable<FieldInfo> GetFields()
+	    {{
+            return this.fields ?? (this.fields = GetType().GetFields(BindingFlags.Instance | BindingFlags.Public)
+                .Where(p => p.GetCustomAttribute(typeof(IgnoreMemberAttribute)) == null)
+                .ToList());
+	    }}
 
-			foreach (var field in GetFields())
-			{{
-				var value = field.GetValue(this);
-				hash = HashValue(hash, value);
-			}}
+	    public override int GetHashCode()
+	    {{
+		    unchecked //allow overflow
+		    {{
+			    int hash = 17;
+			    foreach (var prop in GetProperties())
+			    {{
+				    var value = prop.GetValue(this, null);
+				    hash = HashValue(hash, value);
+			    }}
 
-			return hash;
-		}}
-	}}
+			    foreach (var field in GetFields())
+			    {{
+				    var value = field.GetValue(this);
+				    hash = HashValue(hash, value);
+			    }}
 
-	private int HashValue(int seed, object? value)
-	{{
-		var currentHash = value != null ? value.GetHashCode() : 0;
-		return seed * 23 + currentHash;
-	}}
-}}
+			    return hash;
+		    }}
+	    }}
 
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-public class IgnoreMemberAttribute : Attribute
-{{
+	    private int HashValue(int seed, object? value)
+	    {{
+		    var currentHash = value != null ? value.GetHashCode() : 0;
+		    return seed * 23 + currentHash;
+	    }}
+    }}
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class IgnoreMemberAttribute : Attribute
+    {{
+    }}
 }}";
         }
     }
