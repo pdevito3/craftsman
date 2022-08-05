ef91281d67b11835ab5552ef4b03190d9ab6f8d0
Paul DeVito <pdevito3@gmail.com>
2022-08-05 00:46:43 -0400

    update: registration

--- Craftsman/Builders/ExtensionBuilders/ServiceConfigurationBuilder.cs
+++ Craftsman/Builders/ExtensionBuilders/ServiceConfigurationBuilder.cs
@@ -70,6 +70,8 @@ public static class {FileNames.WebAppServiceConfiguration()}
 
         var config = TypeAdapterConfig.GlobalSettings;
         config.Scan(Assembly.GetExecutingAssembly());
+        builder.Services.AddSingleton(config);
+        builder.Services.AddScoped<IMapper, ServiceMapper>();
 
         builder.Services.AddHealthChecks();
         builder.Services.AddSwaggerExtension();
