88249bbbca15d05c7327be19acdf199636f404ef
Kenneth Nilsson <kenneth@kennethnilsson.com>
2023-07-30 21:34:10 +0200

    Update Pulumi.Keycloak version, using image from keycloak.org docs, docker-compose with admin settings, pulumi settings

--- Craftsman/Builders/Projects/AuthServerProjBuilder.cs
+++ Craftsman/Builders/Projects/AuthServerProjBuilder.cs
@@ -31,7 +31,7 @@ public class AuthServerProjBuilder
 
   <ItemGroup>
     <PackageReference Include=""Pulumi"" Version=""3.*"" />
-    <PackageReference Include=""Pulumi.Keycloak"" Version=""4.11.0"" />
+    <PackageReference Include=""Pulumi.Keycloak"" Version=""5.2.1"" />
   </ItemGroup>
 
 </Project>";
